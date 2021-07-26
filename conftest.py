03dfd5ab6fe5417376c649c69ee9937455cf4209
Emre Abanozoğlu <emre.abanozoglu@gmail.com>
2021-07-26 10:29:50 +0300

    adding files

---
+++ conftest.py
@@ -0,0 +1,22 @@
+# conftest.py
+from pathlib import Path
+from slugify import slugify
+import pytest
+
+
+@pytest.fixture(scope="session")
+def browser_context_args(browser_context_args, playwright):
+    iphone_11 = playwright.devices['iPhone 11 Pro']
+    return {
+        **browser_context_args,
+        **iphone_11,
+    }
+
+
+def pytest_runtest_makereport(item, call) -> None:
+    if call.when == "call":
+        if call.excinfo is not None and "page" in item.funcargs:
+            page = item.funcargs["page"]
+            screenshot_dir = Path(".playwright-screenshots")
+            screenshot_dir.mkdir(exist_ok=True)
+            page.screenshot(path=str(screenshot_dir / f"{slugify(item.nodeid)}.png"))
---
+++ test_pw.py
@@ -0,0 +1,7 @@
+# test_my_application.py
+def test_example_is_working(page):
+    page.goto("https://example.com")
+    assert page.inner_text('h1') == 'Example Dmkkomain'
+    breakpoint()
+    page.wait_for_timeout(3000)
+    page.click("text=More information")
